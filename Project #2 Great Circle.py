d5007c5005aefd5615cc6b50192ca395e6d7c919
unc-fox <tfox1@ualr.edu>
2025-02-10 16:36:53 +0000

    update

--- Project #2 Great Circle.py
+++ Project #2 Great Circle.py
@@ -1,10 +1,10 @@
-import  math
+from math import acos, sin, cos, pi
 print ("The Great Circle Calculator")
-
-
 radius = float (input("Enter Radius of Sphere:  "))
-lat = float (input("Enter Starting point Latitude: "))
-lon = float (input("Enter Starting Point Longitude: "))
-endlat = float (input("Enter Ending Point Latitude: "))
-endlon = float (input("Enter Ending Point Longitude:  "))
-distance = 
+x1 = float (input("Enter Starting point Latitude: ")) * pi / 180.0
+y1 = float (input("Enter Starting Point Longitude: ")) * pi / 180.0
+x2 = float (input("Enter Ending Point Latitude: ")) * pi / 180.0
+y2 = float (input("Enter Ending Point Longitude:  ")) * pi / 180.0
+distance = radius * acos( sin (x1)* sin (x2) + cos(x1) * cos (x2)* cos(y1 - y2) )
+print (round(distance,2))
+
