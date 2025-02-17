bd0742a6b5d1901b5d66d0cb19a58d1d1a3ab424
unc-fox <tfox1@ualr.edu>
2025-02-17 15:53:17 +0000

    Update

---
+++ Project 04.py
@@ -0,0 +1,5 @@
+x = int (input("Enter Maximum Height: "))
+for i in range(1,x+1):
+    print ("*" * i) 
+for i in range (x -1,0,-1):
+    print ("*" *i)
--- goofin.py
+++ goofin.py
@@ -1,14 +1,7 @@
-first = float (input("Input First Number: "))
-operator = input ("Input Operator: ")
-second = float (input("Input Second Number: "))
-if operator == "+":
-    answer = first + second
-elif operator == "-":
-    answer = first - second
-elif operator == "*":
-    answer = first * second 
-elif operator == "/":
-    answer = first / second
-elif operator != "+" "-"  "*" "/":
-    answer = ("Invalid Operator")
-print (first, operator, second, "=", answer) 
+sum = 0
+n = int (input("Enter a number:"))
+for i in range(1, n + 1):
+    sum += i
+    # this ^^ is the shorthand for
+    # sum = sum + i
+print(sum)
